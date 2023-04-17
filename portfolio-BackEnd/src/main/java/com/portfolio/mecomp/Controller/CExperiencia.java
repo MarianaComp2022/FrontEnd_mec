0aa88775a365646f40a48228e3f8b5111e4363b1
Mariana <118371386+MarianaComp2022@users.noreply.github.com>
2023-04-17 00:31:47 -0300

    Correccion de Experiencia y Comienzos de Back Edu

--- portfolio-BackEnd/src/main/java/com/portfolio/mecomp/Controller/CExperiencia.java
+++ portfolio-BackEnd/src/main/java/com/portfolio/mecomp/Controller/CExperiencia.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,15 +47,17 @@ public class CExperiencia {
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoExperiencia dtoexp){
         //Validamos si existe el ID
-        if(!sExperiencia.existsById(id))
+        if(!sExperiencia.existsById(id)){
             return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
         //Compara nombres de Experiencias
-        if(sExperiencia.existsByNombreExp(dtoexp.getNombreExp())&& sExperiencia.getByNombreExp(dtoexp.getNombreExp()).get().getId() != id)
+        if(sExperiencia.existsByNombreExp(dtoexp.getNombreExp())&& sExperiencia.getByNombreExp(dtoexp.getNombreExp()).get().getId() != id){
             return new ResponseEntity(new Mensaje("Esa Experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        }
         //El campo no puede estar vacio
-        if(StringUtils.isBlank(dtoexp.getNombreExp()))
+        if(StringUtils.isBlank(dtoexp.getNombreExp())){
             return new ResponseEntity(new Mensaje("El Nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-        
+        }
         Experiencia experiencia = sExperiencia.getOne(id).get();
         experiencia.setNombreExp(dtoexp.getNombreExp());
         experiencia.setDescripcionExp(dtoexp.getDescripcionExp());
@@ -63,7 +66,15 @@ public class CExperiencia {
         return new ResponseEntity(new Mensaje("Experiencia Actualizada"), HttpStatus.OK);
            
     }
-    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Experiencia> getById(@PathVariable("id") int id) {
+           if(!sExperiencia.existsById(id)){
+               return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+           }
+           Experiencia experiencia = sExperiencia.getOne(id).get();
+           return new ResponseEntity(experiencia, HttpStatus.OK);
+    }   
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         //Validamos si existe el ID
         if(!sExperiencia.existsById(id))
--- portfolio-FrontEnd/src/app/service/s-experiencia.service.ts
+++ portfolio-FrontEnd/src/app/service/s-experiencia.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { Experiencia } from '../model/experiencia';
 
 
+
 @Injectable({
   providedIn: 'root'
 })

dc52cf1f2656854723c389f2a3610fca83c91452
Mariana <118371386+MarianaComp2022@users.noreply.github.com>
2023-04-29 18:57:28 -0300

    edit acerca de correcion ok foto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'mecfrontend',
+    appId: '1:901023085648:web:2c37765c38ecff46dc0d55',
+    storageBucket: 'mecfrontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBUJVQTpRoZtjfK5VGxgfun5yYB6fqe26g',
+    authDomain: 'mecfrontend.firebaseapp.com',
+    messagingSenderId: '901023085648',
+    measurementId: 'G-T2QBR56W8V',
+  },
     production: false,
     URL: 'http://localhost:8080/'
   };
